4f8b163628a57c4b47df2e9667a1f4242ea4cbdd
AndresG0C <andresgonzalez22004@gmail.com>
2026-02-21 22:56:45 -0500

    Creación del mock para ventas temporal

--- InventorySalesFront/src/pages/sales.jsx
+++ InventorySalesFront/src/pages/sales.jsx
@@ -1,5 +1,25 @@
-const Sales = () => {
-    return <h1>Sales</h1>
-}
+import { useState } from "react";
+import SalesTable from "./Components/SalesTable";
+import SaleDetailsModal from "./Components/SaleDetailsModal";
 
-export default Sales
+const mockSales = [
+  {
+    id: "F001",
+    cliente: "Juan Pérez",
+    total: 150000,
+    productos: [
+      { productId: "P01", cantidad: 2, unitPrice: 30000 },
+      { productId: "P02", cantidad: 3, unitPrice: 30000 }
+    ]
+  },
+  {
+    id: "F002",
+    cliente: "María Gómez",
+    total: 80000,
+    productos: [
+      { productId: "P03", cantidad: 1, unitPrice: 80000 }
+    ]
+  }
+];
+
+export default Sales;
