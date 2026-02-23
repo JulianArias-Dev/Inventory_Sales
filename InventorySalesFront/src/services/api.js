16a8d8456ed267fd9df255c12c451e0e0be6c83a
AndresG0C <andresgonzalez22004@gmail.com>
2026-02-22 23:26:02 -0500

    Actualización cnx front-back en docker

--- InventorySalesFront/src/services/api.js
+++ InventorySalesFront/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+// Para desarrollo local (comenta esta línea cuando subas a Docker)
+// const api = axios.create({
+//     baseURL: "http://localhost:5000/api",
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// });
+
+
+// Para Docker
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "http://api:8080/api",
     headers: {
         "Content-Type": "application/json"
     }
