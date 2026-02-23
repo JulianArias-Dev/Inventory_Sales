1e1f9f34557338a90e1b8cde93fc8bdd4f0cc6e6
AndresG0C <andresgonzalez22004@gmail.com>
2026-02-22 21:16:21 -0500

    Corrección del Layout en el app.jsx

--- InventorySalesFront/src/App.jsx
+++ InventorySalesFront/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/layout"
+import Layout from "./components/Layout"
 import Dashboard from "./pages/dashboard"
 import Products from "./pages/products"
 import Categories from "./pages/categories"
